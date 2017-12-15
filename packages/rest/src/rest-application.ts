13b97121fffaf1953cc34f15230f9896ef65c3ff
Kevin Delisle <delisle@ca.ibm.com>
2017-12-15 16:01:20 -0500

    feat(rest): Add sequence and handler to RestApplication API

--- packages/rest/src/rest-application.ts
+++ packages/rest/src/rest-application.ts
@@ -1,12 +1,17 @@
 import {Application, ApplicationConfig, Server} from '@loopback/core';
-import {RestComponent} from './index';
+import {RestComponent, SequenceHandler, SequenceFunction} from './index';
 import {Binding, Constructor} from '@loopback/context';
 import {format} from 'util';
+import {RestBindings} from './keys';
 
 export const ERR_NO_MULTI_SERVER = format(
   'RestApplication does not support multiple servers!',
   'To create your own server bindings, please extend the Application class.',
 );
+
+// To help cut down on verbosity!
+export const SequenceActions = RestBindings.SequenceActions;
+
 /**
  * An implementation of the Application class that automatically provides
  * an instance of a REST server. This application class is intended to be
@@ -35,5 +40,16 @@ export class RestApplication extends Application {
       throw new Error(ERR_NO_MULTI_SERVER);
     }
     return super.server(server, name);
+  }
+
+  sequence(sequence: Constructor<SequenceHandler>): Binding {
+    return this.bind(RestBindings.SEQUENCE).toClass(sequence);
+  }
+
+  handler(handlerFn: SequenceFunction) {
+    // FIXME(kjdelisle): I attempted to mimic the pattern found in RestServer
+    // with no success, so until I've got a better way, this is functional.
+    const server = this.getSync('servers.RestServer');
+    server.handler(handlerFn);
   }
 }
--- packages/rest/test/acceptance/bootstrapping/rest.acceptance.ts
+++ packages/rest/test/acceptance/bootstrapping/rest.acceptance.ts
@@ -12,6 +12,7 @@ import {
   RestApplication,
 } from '../../..';
 import {Application} from '@loopback/core';
+import {ServerResponse, ServerRequest} from 'http';
 
 describe('Bootstrapping with RestComponent', () => {
   context('with a user-defined sequence', () => {
@@ -39,20 +40,22 @@ describe('Bootstrapping with RestComponent', () => {
 });
 
 describe('Starting the application', () => {
-  it('starts an HTTP server', async () => {
+  it('starts an HTTP server (using RestServer)', async () => {
     const app = new Application({
       components: [RestComponent],
       rest: {
         port: 0,
       },
     });
-
+    const server = await app.getServer(RestServer);
+    server.handler(sequenceHandler);
     await startServerCheck(app);
   });
 
-  it('starts an HTTP server', async () => {
+  it('starts an HTTP server (using RestApplication)', async () => {
     const app = new RestApplication();
     app.bind(RestBindings.PORT).to(0);
+    app.handler(sequenceHandler);
     await startServerCheck(app);
   });
 });
@@ -61,10 +64,6 @@ describe('Starting the application', () => {
 // works.
 async function startServerCheck(app: Application) {
   const server = await app.getServer(RestServer);
-  server.handler((sequence, request, response) => {
-    sequence.send(response, 'hello world');
-  });
-
   await app.start();
   const port = await server.get(RestBindings.PORT);
 
@@ -72,4 +71,12 @@ async function startServerCheck(app: Application) {
     .get('/')
     .expect(200, 'hello world');
   await app.stop();
+}
+
+function sequenceHandler(
+  sequence: DefaultSequence,
+  request: ServerRequest,
+  response: ServerResponse,
+) {
+  sequence.send(response, 'hello world');
 }
